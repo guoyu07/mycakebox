faf1af2d98ed6dda145a2295b306fdb223350ebf
chiseledCoder <nikhil.salome@gmail.com>
2018-02-22 15:04:30 +0530

    custom context processor host fix

--- mycakebox/context_processors.py
+++ mycakebox/context_processors.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
 
-def google_map_api():
+def google_map_api(request):
 	""" Returns google map api from settings """
 	return {
 		'GOOGLE_MAPS_API' : settings.GOOGLE_MAPS_API,
